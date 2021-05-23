95563f7a2bf3a4819e8c7b1eabec7c8e8becc1e9
eccentricality <youngmin13@gmail.com>
2021-05-22 22:50:38 -0400

    got font awesome to work for finishing touches, all functions working

--- assets/scheduler.js
+++ assets/scheduler.js
@@ -1,3 +1,4 @@
+// displays today's time at the top
 let todayHeader = moment();
 $('#todayInScheduler').text(todayHeader.format("MMM Do, YYYY"));
 
@@ -8,114 +9,139 @@ let todaysTimes = [
         scheduleTime: '07:00',
         actualTime: '07',
         amPm: ' AM',
-        notes: ''
+        information: ''
     },
     {
         id: '1',
         scheduleTime: '08:00',
         actualTime: '08',
         amPm: ' AM',
-        notes: ''
+        information: ''
     },
     {
         id: '2',
         scheduleTime: '09:00',
         actualTime: '09',
         amPm: ' AM',
-        notes: ''
+        information: ''
     },
     {
         id: '3',
         scheduleTime: '10:00',
         actualTime: '10',
         amPm: ' AM',
-        notes: ''
+        information: ''
     },
     {
         id: '4',
         scheduleTime: '11:00',
         actualTime: '11',
         amPm: ' AM',
-        notes: ''
+        information: ''
     },
     {
         id: '5',
         scheduleTime: '12:00',
         actualTime: '12',
         amPm: ' PM',
-        notes: ''
+        information: ''
     },
     {
         id: '6',
         scheduleTime: '01:00',
         actualTime: '13',
         amPm: ' PM',
-        notes: ''
+        information: ''
     },
     {
         id: '7',
         scheduleTime: '02:00',
         actualTime: '14',
         amPm: ' PM',
-        notes: ''
+        information: ''
     },
     {
         id: '8',
         scheduleTime: '03:00',
         actualTime: '15',
         amPm: ' PM',
-        notes: ''
+        information: ''
     },
     {
         id: '9',
         scheduleTime: '04:00',
         actualTime: '16',
         amPm: ' PM',
-        notes: ''
+        information: ''
     },
     {
         id: '10',
         scheduleTime: '05:00',
         actualTime: '17',
         amPm: ' PM',
-        notes: ''
+        information: ''
     },
     {
         id: '11',
         scheduleTime: '06:00',
         actualTime: '18',
         amPm: ' PM',
-        notes: ''
+        information: ''
     },
     {
         id: '12',
         scheduleTime: '07:00',
         actualTime: '19',
         amPm: ' PM',
-        notes: ''
+        information: ''
     },
     {
         id: '13',
         scheduleTime: '08:00',
         actualTime: '20',
         amPm: ' PM',
-        notes: ''
+        information: ''
     },
     {
         id: '14',
         scheduleTime: '09:00',
         actualTime: '21',
         amPm: ' PM',
-        notes: ''
+        information: ''
     },
     {
         id: '15',
         scheduleTime: '10:00',
         actualTime: '22',
         amPm: ' PM',
-        notes: ''
+        information: ''
     }
 ]
+
+
+// function to store information to local storage
+function storeInformation() {
+    localStorage.setItem('todaysTimes', JSON.stringify(todaysTimes));
+}
+
+// function to call information stored 
+function callInformation() {
+    todaysTimes.forEach(function(_thisTime) {
+        $(`#${_thisTime.id}`).val(_thisTime.information);
+    })
+}
+
+// sets initial state of scheduler depending on stored data
+function setInitialState() {
+    let retrievedInformation = JSON.parse(localStorage.getItem('todaysTimes'));
+
+    if (retrievedInformation) {
+        todaysTimes = retrievedInformation;
+    }
+
+    storeInformation();
+    callInformation();
+}
 
 // dynamically create and append actual scheduler
 todaysTimes.forEach(function(thisTime) {
@@ -150,10 +176,23 @@ todaysTimes.forEach(function(thisTime) {
         todaysPlans.attr({'class': 'future'});
     }
 
-    // create a save button to store data into local storage
-    let saveInformation = $('<i class="far fa-save fa-lg"></i>')
+    // create a save button to store data into local storage add icon to save button
+    let saveInformation = $('<i class="fas fa-archive fa-2x"></i>');
     let saveButton = $('<button>').attr({'class': 'col-md-2 saveButton'});
 
     saveButton.append(saveInformation);
     timeRow.append(schedulerTime, schedulerInfo, saveButton);
+})
+
+setInitialState();
+
+// save button function on click to store data and prevent default so local storage is not lost
+$('.saveButton').on('click', function(event) {
+    event.preventDefault();
+
+    // stores and retrieves stored information based on index per each form row
+    let storedInfoIndex = $(this).siblings('.information').children('.future, .present, .past').attr('id');
+    todaysTimes[storedInfoIndex].information = $(this).siblings('.information').children('.future, .present, .past').val();
+    storeInformation();
+    callInformation();
 })
